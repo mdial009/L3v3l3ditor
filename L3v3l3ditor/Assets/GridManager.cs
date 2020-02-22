2ada4c35ac9b08c577fec40b63cc4f49da7a8530
Madany Diallo <madanyx@gmail.com>
2020-02-22 00:48:20 -0500

    Loading Old Assets

---
+++ L3v3l3ditor/Assets/GridManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField]//SerializeField allows us to change the amount of rows and cols in unity.
+    private int rows = 2;
+    [SerializeField]
+    private int cols = 2;
+    [SerializeField]
+    private float tileSize = 1;// Manage the spacing between items.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();  
+    }
+
+    private void GenerateGrid()
+     {
+
+     GameObject referenceTile = (GameObject)Instantiate(Resources.Load("GrassTile"));
+      
+         for(int row = 0; row < rows; row++)
+         {
+             for(int col = 0; col < cols; col++)
+           {
+               GameObject tile= (GameObject)Instantiate(referenceTile, transform);// Takes Gameobject referenceTile and fills each row and col with the game object.
+
+               float posX = col * tileSize;// places picture into cols
+               float posY = row * -tileSize;// places picture into rows
+                
+               tile.transform.position= new Vector2(posX,posY);
+
+            }
+
+        }
+
+        Destroy(referenceTile);
+
+        float gridW = cols * tileSize;
+        float gridH = rows * tileSize;
+
+        transform.position = new Vector2(-gridW / 2 + tileSize / 2, gridH / 2 - tileSize / 2);// Positions pivet point to center.
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ L3v3l3ditor/Assets/Scenes/LoadScenes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+    public void GotoMainMenu(){
+        SceneManager.LoadScene(0);
+    }
+    public void GoToRegister(){
+        SceneManager.LoadScene(1);
+    }
+    public void GoToLogin(){
+        SceneManager.LoadScene(2);
+    }
+    public void GoToGame(){
+        SceneManager.LoadScene(3);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
