f80a8863215e8d680b4f785e787a4e4217e76d38
morrisnorelus <mpire1997@gmail.com>
2020-03-04 21:15:51 -0500

    Grid maker changes

---
+++ L3v3l3ditor/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TbsFramework.Grid;
+using TbsFramework.Cells;
+using TbsFramework.Gui;
+using TbsFramework.Players;
+using TbsFramework.Units;
+using TbsFramework.Grid.UnitGenerators;
+using TbsFramework.EditorUtils.GridGenerators;
+
+
+public class SceneManager : MonoBehaviour
+{
+    GridHelper Ghelper;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ClickGenerate()
+    {
+        GHelper.GenerateBaseStructure();
+    }
+}
