59457e1cde3d22d7cb88a97ae0815ae3d6c9ccac
Madany Diallo <madanyx@gmail.com>
2020-03-09 04:40:50 -0400

    making new menu

---
+++ L3v3l3ditor/Assets/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame  () {
+        GameObject loading = new GameObject("LoadingText");
+        loading.transform.SetParent(this.transform);
+        loading.AddComponent<Text>().text = "Loading...";
+        Text loadingText = loading.GetComponent<Text>();
+        loadingText.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+        loadingText.transform.position = new Vector3(522, -21);
+        loadingText.fontSize = 22;
+        loadingText.alignment = TextAnchor.UpperCenter;
+        Instantiate(loading);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame (){
+        Debug.Log ("QUIT!");
+        Application.Quit();
+    }
+}
