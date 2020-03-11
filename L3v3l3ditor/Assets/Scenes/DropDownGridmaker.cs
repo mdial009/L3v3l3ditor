c2c9b1f5e9c30c3d40ce131530a4190efe9e9feb
Madany Diallo <madanyx@gmail.com>
2020-03-11 00:18:26 -0400

    update

---
+++ L3v3l3ditor/Assets/Scenes/DropDownGridmaker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class DropDownGridmaker : MonoBehaviour
+{
+    List<string> names = new List<string>() { "1", "2", "3"};
+    public TMPro.TMP_Dropdown dropdown;
+    public Text selectedName; 
+    
+    public void Dropdown_IndexChanged(int index)
+    {
+        selectedName.text = names[index];
+    }
+
+    void Start()
+    {
+     PopulateList();
+    }
+    void PopulateList()
+    {
+    dropdown.AddOptions(names);
+    }
+}
