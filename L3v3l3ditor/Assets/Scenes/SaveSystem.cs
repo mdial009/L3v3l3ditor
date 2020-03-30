e16603251619d72319dbcdf32f867a84c25b07bc
Madany Diallo <madanyx@gmail.com>
2020-03-30 09:40:25 -0400

    updates

--- L3v3l3ditor/Assets/Scenes/SaveSystem.cs
+++ L3v3l3ditor/Assets/Scenes/SaveSystem.cs
@@ -1,21 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Runtime.Serialization.Formatters.Binary;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using System.Runtime.Serialization.Formatters.Binary;
 
-public static class SaveSystem 
-{
-    public static void SavePlayer (Data Data)
-    {
-        BinaryFormatter formatter = new BinaryFormatter();
-        string path = Application.persistentDataPath + "/player.L3v3l3ditor";
-        FileStream stream = new FileStream(path, FileMode.Create);
+// public static class SaveSystem 
+// {
+//     public static void SavePlayer (Data Data)
+//     {
+//         BinaryFormatter formatter = new BinaryFormatter();
+//         string path = Application.persistentDataPath + "/player.L3v3l3ditor";
+//         FileStream stream = new FileStream(path, FileMode.Create);
 
-        PlayerData data = new PlayerData(Data);
+//         PlayerData data = new PlayerData(Data);
         
-        formatter.Serialize(stream, data);
-        stream.Close();
-    }
+//         formatter.Serialize(stream, data);
+//         stream.Close();
+//     }
     
     
-}
+// }
