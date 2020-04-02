5002aa4ec776b6c3487d369797de7d6736494878
Madany Diallo <madanyx@gmail.com>
2020-04-01 22:07:19 -0400

    made a remember me button

--- L3v3l3ditor/Assets/Scenes/RememberMe.cs
+++ L3v3l3ditor/Assets/Scenes/RememberMe.cs
@@ -1,18 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
 
-public class RememberMe : MonoBehaviour
+public class RememberMe : MonoBehaviour {
+
+//reference for InputField, needs to be assigned in editor
+public TMPro.TMP_InputField login;
+string loginText;
+//reference for Toggle, needs to be assigned in editor
+public Toggle rememberToggle;
+bool remeberIsOn;
+
+// Use this for initialization
+void Start ()
 {
-    // Start is called before the first frame update
-    void Start()
+    loginText = "";
+    FindLogin();
+}
+public void FindLogin()
+{
+    if (File.Exists("userSetting.txt"))
     {
-        
+        using (TextReader reader = File.OpenText("userSetting.txt"))
+        {
+            string str;
+            if ((str = reader.ReadLine()) != null) login.text = str;
+        }
+        //Debug.Log(loginText);
     }
-
-    // Update is called once per frame
-    void Update()
+}
+public void remember()
+{
+    using (TextWriter writer = File.CreateText("userSetting.txt"))
     {
-        
+        writer.WriteLine(login.text);
     }
+}
+public void loginClick()
+{
+    remeberIsOn = rememberToggle.isOn;
+    loginText = login.text;
+    if (remeberIsOn)
+    {
+        remember();
+    }
+    else
+    {
+        using (TextWriter writer = File.CreateText("userSetting.txt"))
+        {
+            writer.WriteLine("");
+        }
+    }
+}
 }
