e63caeaf0d8b572439026e7105a08c3e2471c155
morrisnorelus <mpire1997@gmail.com>
2020-04-01 21:14:19 -0400

    changes

---
+++ L3v3l3ditor/Assets/Scenes/Dimensions.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dimensions : MonoBehaviour
+{
+
+    public static int rows = 1;
+    public static int cols = 1;
+
+    public static int players = 1;
+
+    List<string> names = new List<string>() { "1", "2", "3", "4", "5", "6", "7", "8","9", "10" };
+
+    public Dropdown dropdown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PopulateList();
+    }
+
+
+
+    void PopulateList()
+    {
+        dropdown.AddOptions(names);
+
+    }
+
+    public void OnDropDownChangedRows(Dropdown dropDown)
+    {
+        Debug.Log("Rows -> " + (dropDown.value + 1));
+        rows = dropDown.value + 1;
+    }
+
+    public void OnDropDownChangedCols(Dropdown dropDown)
+    {
+        Debug.Log("Columns -> " + (dropDown.value + 1));
+        cols = dropDown.value + 1;
+    }
+
+    public void OnDropDownChangedPlayers(Dropdown dropDown)
+    {
+        Debug.Log("Players -> " + (dropDown.value + 1));
+        players = dropDown.value + 1;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
