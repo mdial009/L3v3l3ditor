abdf7089b2e521aa498053728e19284cff6a6e7c
Madany Diallo <madanyx@gmail.com>
2020-04-20 21:58:03 -0400

    GameScreen(Checked)
    FInished making the Game Screen.

---
+++ L3v3l3ditor/Assets/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public void DisableBoolAnimator(Animator anime)
+    {
+        anime.SetBool("IsDisplayed", false);
+    }
+    public void EnableBoolAnimator(Animator anime)
+    {
+        anime.SetBool("IsDisplayed", true);
+    }
+    public void NavigateTo(int scene)
+    {
+        Application.LoadLevel(scene);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    
+}
