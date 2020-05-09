1d71f8f19b6812a983b881633e660eb01c5b85a8
Madany Diallo <madanyx@gmail.com>
2020-05-08 23:14:16 -0400

    Update Dimensions.cs

--- L3v3l3ditor/Assets/Scenes/Scripts/Dimensions.cs
+++ L3v3l3ditor/Assets/Scenes/Scripts/Dimensions.cs
@@ -11,7 +11,7 @@ public class Dimensions : MonoBehaviour
 
     public static int players = 1;
 
-    List<string> names = new List<string>() { "1", "2", "3", "4", "5", "6", "7", "8","9", "10" };
+    List<string> names = new List<string>() { "1", "2"};
 
 
     public Dropdown dropdown;
